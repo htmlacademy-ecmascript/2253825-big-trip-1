7ac58a1e29351e02b408bf7b25718ac621668417
Midnightess <asm1@bk.ru>
2024-02-14 13:38:35 +0300

    Исправила ошибки, вернула 1 презентер

--- src/main.js
+++ src/main.js
@@ -1,19 +1,11 @@
 import FiltersPresenter from './presenter/filters-presenter.js';
-import TripEventsPresenter from './presenter/trip-events-presenter.js';
 
-
-const siteBodyElement = document.querySelector('.page-header');
-const siteTripInfo = siteBodyElement.querySelector('.trip-main');
-const tripFilterHeader = siteBodyElement.querySelector('.trip-controls__filters');
-
-
-const filtersPresenter = new FiltersPresenter({ tripInfoContainer: siteTripInfo, tripFilterContainer: tripFilterHeader });
 
 const siteMainElement = document.querySelector('.page-main');
 const tripMainEvents = siteMainElement.querySelector('.trip-events');
 
-const tripEventsPresenter = new TripEventsPresenter({ tripEventsContainer: tripMainEvents });
+const filtersPresenter = new FiltersPresenter ({ tripFilterContainer: tripMainEvents });
 
 
 filtersPresenter.init();
-tripEventsPresenter.init();
+
--- src/presenter/filters-presenter.js
+++ src/presenter/filters-presenter.js
@@ -3,20 +3,34 @@ import { render } from '../render.js';
 
 import FilterTimeView from '../view/filter-time-view.js';
 import HeaderTravelView from '../view/header-travel-view.js';
+import TripSortView from '../view/trip-sort-view.js';
+import FormEventView from '../view/form-event-view.js';
+import FormEditEvent from '../view/form-edit-event-view.js';
+import TripListView from '../view/trip-list-view.js';
 
+const WAYPOINT = 3;
 
 export default class FiltersPresenter {
   tripInfoComponent = new HeaderTravelView();
   tripFilterComponent = new FilterTimeView();
+  tripSortComponent = new TripSortView();
+  tripEventsComponent = new FormEventView();
 
 
   constructor ({ tripFilterContainer }) {
     this.tripFilterContainer = tripFilterContainer;
   }
 
-
   init () {
     render(this.tripInfoComponent, this.tripFilterContainer, RenderPosition.AFTERBEGIN);
     render(this.tripFilterComponent, this.tripFilterContainer, RenderPosition.AFTERBEGIN);
+    render(this.tripSortComponent, this.tripFilterContainer);
+    render(this.tripEventsComponent, this.tripFilterContainer);
+    render(new FormEditEvent(),this.tripEventsComponent.getElement());
+
+    for (let i = 0; i < WAYPOINT; i++) {
+      render(new TripListView, this.tripEventsComponent.getElement());
+    }
   }
 }
+
