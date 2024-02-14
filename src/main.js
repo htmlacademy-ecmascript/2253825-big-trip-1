04cb9e5b6296030f99498d6983190491e5a7924a
Midnightess <asm1@bk.ru>
2024-02-14 14:54:56 +0300

    Еще раз вернула 2 презентер

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import FiltersPresenter from './presenter/filters-presenter.js';
+import MainPresenter from './presenter/main-presenter.js';
+
+
+const siteBodyElement = document.querySelector('.page-header');
+const siteTripInfo = siteBodyElement.querySelector('.trip-main');
 
 
 const siteMainElement = document.querySelector('.page-main');
 const tripMainEvents = siteMainElement.querySelector('.trip-events');
 
-const filtersPresenter = new FiltersPresenter ({ tripFilterContainer: tripMainEvents });
+const filtersPresenter = new FiltersPresenter ({ tripFilterContainer: siteTripInfo });
+const mainPresenter = new MainPresenter ({ tripMainContainer: tripMainEvents });
 
 
 filtersPresenter.init();
+mainPresenter.init();
 
